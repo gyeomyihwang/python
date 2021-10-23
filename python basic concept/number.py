91669b2dc23d763fd336c1cb4a926d239d735416
Gyeomyi Hwang <gyeomyihwangkr@gmail.com>
2021-10-23 11:47:06 +0900

    Create number.py

---
+++ python basic concept/number.py
@@ -0,0 +1,37 @@
+# Number - Integer (정수형)
+a = 1
+a = 3
+a -5
+print(type(a)) # int
+
+# Number - Floating point (실수형)
+a = 1.5
+a = -3.1
+print(type(a)) # float
+
+# calculating 
+a = 6
+b = 2
+
+# addition
+print(a + b) # 8 
+# multiplication
+print(a * b) # 12 
+# division
+print(a / b) # 3.0 (division results are always float)
+# exponentiation
+print(a ** b) # 36 
+
+# modulus (나머지값)
+a = 7
+b = 3
+
+print(a % b) # 1
+print(b % a) # 3
+
+# floor division
+a = 7
+b = 4
+
+print(a / b) # 1.75
+print(a // b) # 1
