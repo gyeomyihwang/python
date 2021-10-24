65d670a10bd56b423db4756944a5130f53624b30
Gyeomyi Hwang <gyeomyihwangkr@gmail.com>
2021-10-24 14:44:04 +0900

    Create boolean.py

---
+++ python basic concept/boolean.py
@@ -0,0 +1,10 @@
+# Boolean (True or False)
+a = True
+b = False
+print(type(a)) # <class 'bool'>
+print(type(b)) # <class 'bool'>
+
+# 조건문의 반환 값으로도 사용됨
+print(1 == 1) # True => 1과 1은 같은 값인가? 
+print(2 > 1) # True
+print(2 < 1) # False
