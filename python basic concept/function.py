abb0af0b0ed54c5aae53ab9e237fd267be275ed8
Gyeomyi Hwang <gyeomyihwangkr@gmail.com>
2021-10-27 00:03:21 +0900

    Update function.py

--- python basic concept/function.py
+++ python basic concept/function.py
@@ -4,7 +4,17 @@
 #     <수행할 문장>
 #     <수행할 문장>
 
-# 예시 1.
+# 예시 1. 함수 이름은 add이고 입력으로 2개 값을 받으며 결괏값은 2개의 입력값을 더한 값이다.
 def add(a,b):
-    return a + b
-print(add(1,2)) # 3
+    return a + b # 함수의 결괏값을 돌려주는 명령어
+print(add(1,2)) # 3 
+
+# 예시 2. 예시 1과 같은 함수
+def add(a, b):
+    return a+b
+a = 3
+b = 7
+c = add(a,b)
+print(c) # 10
+
+# 매개변수와 인수
